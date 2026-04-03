b9ae521c24c0e87b66cfa02f0ff3dd38b7546785
VarnikaGulhane1 <gulhanevarnika@gmail.com>
2026-04-03 17:26:09 +0530

    list, tuple, set example

---
+++ dict_for/d1.py
@@ -0,0 +1,14 @@
+student = {
+    "name": "Varnika",
+    "age": 22,
+    "course": "MCA"
+}
+
+keyName = 'age'
+
+print(student[keyName])
+
+# Loop through keys(Dynamically allocation)
+# for key in student:
+#     print(f"key = {key}, type of key = {type(key)}, keyValue = {student.get(key)}")
+
